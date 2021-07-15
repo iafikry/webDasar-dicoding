da3f6558b56b897285bff2848aaa4c6e5f0df732
iafikry <iafikry30@gmail.com>
2021-07-15 21:10:33 +0700

    add folder fron-end-dicoding

---
+++ front-end-dicoding/main.js
@@ -0,0 +1,14 @@
+if (score >= 90) {
+  result = "Selamat! Anda mendapatkan nilai A";
+} else if (score >= 80 && score <= 89) {
+  result = "Anda mendapatkan nilai B";
+} else if (score >= 70 && score <= 79) {
+  result = "Anda mendapatkan nilai C";
+} else if (score >= 60 && score <= 69) {
+  result = "Anda mendapatkan nilai D";
+} else if (score < 60) {
+  result = "Anda mendapatkan nilai E";
+}
+
+hasil = scoreChecker(60);
+console.log(hasil);
